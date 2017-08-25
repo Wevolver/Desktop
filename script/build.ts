1b0813ae590940eb3fb782c1a93e708be2f0543e
Rodrigo <rodrigo@wevolver.com>
2017-08-25 10:55:59 +0200

    Change images to wevolver

--- script/build.ts
+++ script/build.ts
@@ -106,7 +106,7 @@ function packageApp(
       new RegExp('/\\.git($|/)'),
       new RegExp('/node_modules/\\.bin($|/)'),
     ],
-    appCopyright: 'Copyright © 2017 GitHub, Inc.',
+    appCopyright: 'Copyright © 2017 Wevolver',
 
     // macOS
     appBundleId: distInfo.getBundleID(),
